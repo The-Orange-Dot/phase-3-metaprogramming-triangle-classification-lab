c1e0c5a0e937e0c92741aa5945e3d339093724c8
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-11 19:39:31 +0900

    Completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    if a <= 0 || b <= 0 || c <= 0
+      begin
+        raise TriangleError
+        puts error.zero_error
+      end
+    elsif a + b <= c || b + c <= a || c + a <= b
+      begin
+        raise TriangleError
+        puts error.ineq_error
+      end
+    elsif a == b && b == c && c == a
+      @triangle = :equilateral
+    elsif a != b && b != c && c != a
+      @triangle = :scalene
+    elsif a && b != c || b && c != a || a && c != b
+      @triangle = :isosceles
+    end
+  end
+
+  def kind
+      @triangle
+  end
+
+  class TriangleError < StandardError
+    def zero_error
+      "triangle class cannot have a side of 0 or less"
+    end
+    def ineq_error
+      "the sum of the length of two sides must be greater than the side of the third"
+    end
+  end
+
+  # binding.pry
 end
